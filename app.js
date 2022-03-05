88ee6a76f62d2078b7638fd4e37d1daa4a6c1476
Grant Harris <grant@grantharris.ca>
2022-03-05 12:23:18 -0800

    Working first pass

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const bodyParser = require('body-parser');
+const routes = require("./routes");
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(bodyParser.json());
+app.use(routes);
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+	cors: {
+    	origin: '*',
+ }});
+
+app.set('socketIo', io);
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ routes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require('path');
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+router.post("/deckLoaded/:deck", (req, res) => {
+  let socket = req.app.get('socketIo');
+  socket.emit("deckLoaded", {
+    "client": req.ip,
+    "deck": req.params.deck,
+    "body": req.body
+  });
+
+  res.send().status(200);
+});
+
+router.post("/updateDeck/:deck", (req, res) => {
+  let socket = req.app.get('socketIo');
+  socket.emit("updateDeck", {
+    "client": req.ip,
+    "deck": req.params.deck,
+    "body": req.body
+  });
+
+  res.send().status(200);
+});
+
+router.post("/updateMasterClock", (req, res) => {
+  let socket = req.app.get('socketIo');
+  socket.emit("updateMasterClock", {
+    "client": req.ip,
+    "body": req.body
+  });
+
+  res.send().status(200);
+});
+
+router.post("/updateChannel/:channel", (req, res) => {
+  let socket = req.app.get('socketIo');
+  socket.emit("updateChannel", {
+    "client": req.ip,
+    "channel": req.params.channel,
+    "body": req.body
+  });
+
+  res.send().status(200);
+});
+
+module.exports = router;
